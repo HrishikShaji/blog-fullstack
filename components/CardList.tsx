1fc7de049e7d39b30a319f799318c8ab3053f0e4
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-01 21:59:25 +0530

    today enough

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -3,6 +3,8 @@ import { Pagination } from "./Pagination";
 export const CardList = () => {
   return (
     <div>
+      <h1 className="text-3xl">Recent Posts</h1>
+      <div></div>
       <Pagination />
     </div>
   );
--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export const CategoryList = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-5 p-10">
+      <h1 className="text-xl">Popular categories</h1>
+      <div className="w-full flex justify-center items-center">
+        <div className="flex gap-4">
+          <Link href="/blog?cat=style">Style</Link>
+          <Link href="/blog?cat=style">Fashion</Link>
+          <Link href="/blog?cat=style">Food</Link>
+          <Link href="/blog?cat=style">Travel</Link>
+          <Link href="/blog?cat=style">Coding</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
