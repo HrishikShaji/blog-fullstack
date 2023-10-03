e91f6b97bd12916e9519baaf5cf214fffcb89980
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-03 12:06:41 +0530

    editor done

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,30 +1,37 @@
 "use client";
 
-import { warn } from "console";
 import { useState } from "react";
 import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
 
 const Page = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <input placeholder="Title" />
-      <div>
-        <button>Add</button>
+      <div className="flex h-[700px] flex-col items-start">
+        <button
+          onClick={() => setOpen(!open)}
+          className="px-3 py-2 border-2 border-white"
+        >
+          Add
+        </button>
         {open && (
           <div className="flex gap-2">
-            <button>image</button>
-            <button>file</button>
-            <button>video</button>
+            <button className="px-3 py-2 border-white border-2">image</button>
+            <button className="px-3 py-2 border-white border-2">file</button>
+            <button className="px-3 py-2 border-white border-2">video</button>
           </div>
         )}
         <ReactQuill
+          className="w-full h-full"
           theme="bubble"
           value={value}
           onChange={setValue}
           placeholder="Tell your story..."
         />
+        <button className="px-3 py-2 border-white border-2">Publish</button>
       </div>
     </div>
   );
