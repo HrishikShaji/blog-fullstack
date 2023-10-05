bc106d87bb344cfaa2035c6b3a61afdf9cda0eff
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-05 19:25:56 +0530

    blog page done

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -18,43 +18,32 @@ const Page = async ({ params }) => {
 
   const data = await getData(slug);
   return (
-    <div className="">
-      <div className="flex items-center gap-[50px]">
-        <div className="flex-1 relative flex flex-col gap-10">
-          <h1 className="text-3xl">{data.title}</h1>
-          <div className="flex items-center gap-[20px]">
-            <Image
-              className="h-20 w-20 rounded-full object-cover"
-              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400"
-              height={1000}
-              width={1000}
-              alt="image"
-            />
-            <div className="relative h-[50px] w-[50px]">
-              <span>{data?.user.email}</span>
-              <span>10.3.2023</span>
-            </div>
-          </div>
-        </div>
-        <div className="flex-1">
-          <Image
-            className="relative h-[350px]"
-            src="https://images.pexels.com/photos/18427797/pexels-photo-18427797/free-photo-of-light-sea-dawn-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            height={1000}
-            width={1000}
-            alt="image"
-          />
+    <div className="p-10 flex flex-col gap-10">
+      <div className="flex gap-2 items-center">
+        <Image
+          className="h-20 w-20 rounded-full object-cover"
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400"
+          height={1000}
+          width={1000}
+          alt="image"
+        />
+        <div className="flex flex-col ">
+          <span className="text-xl font-semibold">{data?.user.email}</span>
+          <span className="text-sm text-gray-400">10.3.2023</span>
         </div>
       </div>
-      <div className="flex gap-[50px]">
-        <div className="flex-[5]">
-          <p>{data.desc}</p>
-          <div>
-            <Comments postSlug={slug} />
-          </div>
-        </div>
-        <Menu />
+      <div className="flex flex-col gap-5">
+        <h1 className="text-3xl font-semibold">{data.title}</h1>
+        <Image
+          className="w-full h-full"
+          src="https://images.pexels.com/photos/18427797/pexels-photo-18427797/free-photo-of-light-sea-dawn-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          height={1000}
+          width={1000}
+          alt="image"
+        />
       </div>
+      <p>{data.desc}</p>
+      <Comments postSlug={slug} />
     </div>
   );
 };
--- components/Comments.tsx
+++ components/Comments.tsx
@@ -41,35 +41,44 @@ export const Comments: React.FC<CommentsProps> = ({ postSlug }) => {
     setDesc("");
   };
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-10 w-full">
       <h1>Comments</h1>
       {status === "authenticated" ? (
-        <div className="flex gap-2">
+        <div className="flex gap-2 w-full">
           <textarea
-            className="text-black"
+            className="w-full bg-transparent border-b-2 text-white focus:outline-none border-white"
             onChange={(e) => setDesc(e.target.value)}
+            value={desc}
           />
-          <button onClick={handleSubmit}>Send</button>
+          <button
+            onClick={handleSubmit}
+            className="px-3 py-2 border-white border-2"
+          >
+            Send
+          </button>
         </div>
       ) : (
         <Link href="/login">Login to write a comment</Link>
       )}
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-6 ">
         {isLoading
           ? "Loading"
           : data?.map((item) => (
-              <div className="flex flex-col gap-4" key={item.id}>
-                <div className="flex gap-2">
+              <div
+                className="flex flex-col gap-2 border-b border-gray-700 pb-5"
+                key={item.id}
+              >
+                <div className="flex gap-2 items-center">
                   <Image
-                    className="h-10 w-10 rounded-full"
+                    className="h-14 w-14 rounded-full"
                     src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=400"
                     alt="image"
                     height={1000}
                     width={1000}
                   />
-                  <div>
-                    <span>{item.user.email}</span>
-                    <span>10.3.2023</span>
+                  <div className="flex flex-col ">
+                    <span className="">{item.user.email}</span>
+                    <span className="text-sm text-gray-400">10.3.2023</span>
                   </div>
                 </div>
                 <div>
