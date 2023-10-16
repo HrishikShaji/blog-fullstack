214d007dfafcd5a10131288c67e5b5e5c2a218e0
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-16 17:57:04 +0530

    ui for comment list started

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -32,50 +32,52 @@ export const Comment: React.FC<CommentProps> = ({ item, postSlug }) => {
 
   return (
     <div
-      className="flex flex-col gap-6 border-b border-gray-700 pt-2 pb-5"
+      className="flex flex-col w-full gap-6 border-b border-gray-700 pt-2 pb-5"
       key={item.id}
     >
-      <div className="flex gap-2 items-start">
-        <Image
-          className="h-14 w-14 rounded-full"
-          src={item.user.image}
-          alt="image"
-          height={1000}
-          width={1000}
-        />
-        <div className="flex flex-col gap-2">
-          <div className="flex gap-2 items-center">
-            <span className="font-semibold">{item.user.email}</span>
-            <span className="text-xs text-gray-400">10.3.2023</span>
-          </div>
-          <div>
-            <p>{item.desc}</p>
-          </div>
-          <div className="flex gap-2">
-            <button
-              onClick={() => setReplies(!replies)}
-              className="py-2 px-3 border-2 border-white"
-            >
-              Reply
-            </button>
+      <div>
+        <div className="flex gap-2 items-start w-full">
+          <Image
+            className="h-14 w-14 rounded-full"
+            src={item.user.image}
+            alt="image"
+            height={1000}
+            width={1000}
+          />
+          <div className="flex flex-col gap-2 w-full">
+            <div className="flex gap-2 items-center">
+              <span className="font-semibold">{item.user.email}</span>
+              <span className="text-xs text-gray-400">10.3.2023</span>
+            </div>
+            <div>
+              <p>{item.desc}</p>
+            </div>
+            <div className="w-full">
+              <button
+                onClick={() => setReplies(!replies)}
+                className="py-1 px-3 border-2 border-white"
+              >
+                {replies ? "Cancel" : "Reply"}
+              </button>
+              {replies && (
+                <form className="w-full flex gap-2">
+                  <input
+                    className="w-full bg-transparent border-b-2 focus:outline-none border-white"
+                    value={reply}
+                    onChange={(e) => setReply(e.target.value)}
+                  />
+                  <button
+                    className="py-1 px-3 border-2 border-white"
+                    onClick={() => handleReply(reply, item.id)}
+                  >
+                    Reply
+                  </button>
+                </form>
+              )}
+            </div>
           </div>
         </div>
       </div>
-      {replies && (
-        <form className="w-full flex gap-2">
-          <input
-            className="w-full bg-transparent border-b-2 border-white"
-            value={reply}
-            onChange={(e) => setReply(e.target.value)}
-          />
-          <button
-            className="py-2 px-3 border-2 border-white"
-            onClick={() => handleReply(reply, item.id)}
-          >
-            Reply
-          </button>
-        </form>
-      )}
     </div>
   );
 };
--- components/CommentList.tsx
+++ components/CommentList.tsx
@@ -16,27 +16,30 @@ export const CommentList: React.FC<CommentListProps> = ({
     Array(comments.length).fill(false),
   );
 
-  const toggleReplies = (index) => {
+  const toggleReplies = (index: number) => {
     const newShowReplies = [...showReplies];
     newShowReplies[index] = !newShowReplies[index];
     setShowReplies(newShowReplies);
   };
 
   return (
-    <div>
-      {comments.map((comment, index) => {
+    <div className="w-full overflow-x-hidden">
+      {comments.map((comment, index: number) => {
         console.log(comment);
         return (
-          <div key={comment.id}>
+          <div
+            key={comment.id}
+            className="border-b-2 pb-4 flex flex-col gap-2 items-start border-white"
+          >
             <Comment item={comment} postSlug={postSlug} />
             <button
               onClick={() => toggleReplies(index)}
-              className="px-3 ml-10 py-2 border-2 border-white"
+              className="px-3  py-1 border-2 border-white"
             >
-              Replies
+              {showReplies[index] ? "Hide Replies" : "Show Replies"}
             </button>
             {showReplies[index] && comment.children.length > 0 && (
-              <div className="ml-10">
+              <div className="ml-10 w-full">
                 <CommentList comments={comment.children} postSlug={postSlug} />
               </div>
             )}
