ecc17706b01562d6b0f9c22792f57d8792fa068a
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-18 18:31:15 +0530

    image preview done

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,14 +8,25 @@ interface CardProps {
 }
 
 export const Card: React.FC<CardProps> = ({ item }) => {
+  const data = JSON.parse(item.content);
+  const images = data.blocks.filter((block) => block.type == "image");
+  console.log("images in ", images);
   return (
-    <div className="flex relative h-[300px] w-full bg-gray-500">
+    <div className="flex relative h-[300px] w-full bg-gray-500 overflow-hidden">
       <div className="absolute z-10 flex flex-col gap-2">
         <div className="flex gap-2">
           <span>{item.createdAt.toString().substring(0, 10)}</span>
         </div>
         <h1 className="text-xl font-bold">{item.title}</h1>
-        <EditorOutput content={item.content} />
+        {images ? (
+          <Image
+            className="object-cover "
+            height={1000}
+            width={1000}
+            alt=""
+            src={images[0].data.file.url}
+          />
+        ) : null}
         <Link href={`/posts/${item.slug}`}>See more</Link>
       </div>
     </div>
