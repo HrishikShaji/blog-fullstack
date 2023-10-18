832e6d840f703ebb7ebb8e31373bc5d6250dd102
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-18 22:12:02 +0530

    featured done

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -14,17 +14,23 @@ const getData = async (page: number) => {
 
 export const Featured = async () => {
   const data = await getData(1);
+
+  const content = JSON.parse(data.posts[0].content);
+  const images = content.blocks.filter((block) => block.type == "image");
+  const image = images.length > 0 ? images[0].data.file.url : null;
   return (
     <div className="flex flex-col gap-10">
       <h1 className="text-4xl">Discover my Stories and creative ideas</h1>
-      <div className="flex gap-4 relative justify-center items-center">
-        <Image
-          className="w-full h-[50vh] object-cover"
-          alt="image"
-          height={1000}
-          width={1000}
-          src={data?.posts[0].img}
-        />
+      <div className="flex gap-4 relative h-[50vh] bg-neutral-600 justify-center items-center">
+        {image && (
+          <Image
+            className="w-full h-full object-cover"
+            alt="image"
+            height={1000}
+            width={1000}
+            src={image}
+          />
+        )}
         <h1 className="absolute z-10 text-4xl font-bold ">
           {data?.posts[0].title}
         </h1>
