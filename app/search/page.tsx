bcfc557d345ea80882c735d3b779f2f6562d9537
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-26 16:42:18 +0530

    partial search done

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,11 +2,14 @@
 
 import { ExtendedPost } from "@/types/Types";
 import { useState } from "react";
+import Image from "next/image";
+import { Link } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
   const [inputValue, setInputValue] = useState("");
   const [results, setResults] = useState([]);
-
+  const router = useRouter();
   const fetchData = async (value) => {
     await fetch("/api/search")
       .then((response) => response.json())
@@ -25,25 +28,52 @@ const Page = () => {
     fetchData(value);
   };
   return (
-    <div className="min-h-screen w-full pt-20 p-10 flex flex-col gap-10 items-center">
-      <div className="flex gap-2">
-        <input
-          className="p-2 border-2 text-black"
-          value={inputValue}
-          onChange={(e) => handleChange(e.target.value)}
-          placeholder="Type to search..."
-        />
-        <button className="py-2 px-3 border-2 border-white text-white">
-          Search
-        </button>
-      </div>
+    <div className="min-h-screen w-full pt-20 p-10 flex flex-col gap-2 items-center">
       <div>
-        {results.map((post: ExtendedPost) => (
-          <div key={post.id}>
-            <h1>{post.slug}</h1>
-            <h1>{post.title}</h1>
-          </div>
-        ))}
+        <div className="flex gap-2">
+          <input
+            className="p-2 border-2 text-black"
+            value={inputValue}
+            onChange={(e) => handleChange(e.target.value)}
+            placeholder="Type to search..."
+          />
+          <button className="py-2 px-3 border-2 border-white text-white">
+            Search
+          </button>
+        </div>
+        <div className="w-full max-h-[300px] overflow-y-scroll flex flex-col gap-2 p-2">
+          {results.map((post: ExtendedPost) => {
+            const content = post.content as any;
+            const images = content.blocks.filter(
+              (block: any) => block.type == "image",
+            );
+            const image = images?.length > 0 ? images[0].data.file.url : null;
+            return (
+              <div
+                key={post.id}
+                onClick={() => router.push(`/posts/${post.slug}`)}
+                className="flex gap-2 cursor-pointer p-1 hover:bg-neutral-600 bg-neutral-800"
+              >
+                {image ? (
+                  <Image
+                    alt="image"
+                    height={1000}
+                    width={1000}
+                    className="h-10 w-10 object-cover"
+                    src={image}
+                  />
+                ) : (
+                  <div className="h-10 w-10 bg-gray-500 flex justify-center items-center">
+                    ?
+                  </div>
+                )}
+                <div>
+                  <h1>{post.title}</h1>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
