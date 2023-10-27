bc5b22d29afda346f6c7be24fbbda9445b8a3f04
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-27 15:51:42 +0530

    dropdown added

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -10,7 +10,11 @@ const Page = () => {
   const [results, setResults] = useState([]);
   const [finalResults, setFinalResults] = useState([]);
   const [suggestions, setSuggestions] = useState(false);
+  const [isSortOpen, setIsSortOpen] = useState(false);
+  const [selectedSort, setSelectedSort] = useState(null);
   const router = useRouter();
+  const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const fetchData = async (value: string) => {
     if (value !== "") {
       console.log(value);
@@ -57,6 +61,8 @@ const Page = () => {
   };
 
   const handleSort = async (sort: string) => {
+    setSelectedSort(sort);
+    setIsSortOpen(false);
     await fetch(`/api/search?sort=${sort}`)
       .then((response) => response.json())
       .then((json) => {
@@ -70,6 +76,8 @@ const Page = () => {
   };
 
   const handleFilter = async (filter: string) => {
+    setSelectedCategory(filter);
+    setIsCategoryOpen(false);
     await fetch(`/api/search?filter=${filter}`)
       .then((response) => response.json())
       .then((json) => {
@@ -146,29 +154,86 @@ const Page = () => {
       </div>
       <div className="w-full">
         {finalResults.length > 0 && (
-          <>
-            <div className="flex flex-col items-center gap-2">
-              <h1>Sort By</h1>
-              <div className="flex gap-2">
-                <button onClick={() => handleSort("createdAt")}>Date</button>
-                <button onClick={() => handleSort("views")}>Views</button>
-                <button onClick={() => handleSort("votes")}>Likes</button>
-              </div>
-            </div>
-            <div className="flex flex-col items-center gap-2">
-              <h1>Filter By</h1>
-              <div className="flex gap-2">
-                <button onClick={() => handleFilter("style")}>Frontend</button>
-                <button onClick={() => handleFilter("travel")}>Backend</button>
-                <button onClick={() => handleFilter("coding")}>UI/UX</button>
-                <button onClick={() => handleFilter("devops")}>DevOps</button>
-                <button onClick={() => handleFilter("ai")}>AI</button>
-                <button onClick={() => handleFilter("blockchain")}>
-                  Blockchain
+          <div className="flex gap-2">
+            <div className="flex flex-col items-center gap-2 w-[200px]">
+              <div className="flex gap-2 border-2 border-white  justify-between p-2 w-full">
+                <h1>{selectedSort || "Sort By"}</h1>
+                <button onClick={() => setIsSortOpen(!isSortOpen)}>
+                  Select
                 </button>
               </div>
+              {isSortOpen && (
+                <div className="flex flex-col bg-neutral-500  w-full">
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleSort("createdAt")}
+                  >
+                    Date
+                  </button>
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleSort("views")}
+                  >
+                    Views
+                  </button>
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleSort("votes")}
+                  >
+                    Likes
+                  </button>
+                </div>
+              )}
             </div>
-          </>
+            <div className="flex flex-col items-center gap-2 w-[200px]">
+              <div className="flex gap-2 border-2 border-white  justify-between p-2 w-full">
+                <h1>{selectedCategory || "Category"}</h1>
+                <button onClick={() => setIsCategoryOpen(!isCategoryOpen)}>
+                  Select
+                </button>
+              </div>
+              {isCategoryOpen && (
+                <div className="flex flex-col bg-neutral-500 w-full">
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleFilter("style")}
+                  >
+                    Frontend
+                  </button>
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleFilter("travel")}
+                  >
+                    Backend
+                  </button>
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleFilter("coding")}
+                  >
+                    UI/UX
+                  </button>
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleFilter("devops")}
+                  >
+                    DevOps
+                  </button>
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleFilter("ai")}
+                  >
+                    AI
+                  </button>
+                  <button
+                    className="border-b-2 border-white py-2"
+                    onClick={() => handleFilter("blockchain")}
+                  >
+                    Blockchain
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
         )}
         {finalResults.map((post: ExtendedPost) => {
           const content = post.content as any;
