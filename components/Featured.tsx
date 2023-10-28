6a7b4a0b7c030eec64dd2f86f98d552e54916237
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-28 23:33:44 +0530

    removed content

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -7,22 +7,10 @@ export const Featured = async () => {
   const { posts } = await fetchPosts(`${baseUrl}/api/posts?page=1`);
 
   if (!posts[0]) return <div>NO POSTS YET...</div>;
-  const content = posts[0]?.content as any;
-  const images = content.blocks.filter((block: any) => block.type == "image");
-  const image = images?.length > 0 ? images[0].data.file.url : null;
   return (
     <div className="flex flex-col gap-10">
       <h1 className="text-4xl">Discover my Stories and creative ideas</h1>
       <div className="flex gap-4 relative h-[50vh] bg-neutral-600 justify-center items-center">
-        {image && (
-          <Image
-            className="w-full h-full object-cover"
-            alt="image"
-            height={1000}
-            width={1000}
-            src={image}
-          />
-        )}
         <h1 className="absolute z-10 text-4xl font-bold ">{posts[0]?.title}</h1>
         <Link href={`posts/${posts[0]?.slug}`}>Read more</Link>
       </div>
