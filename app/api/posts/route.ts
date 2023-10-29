de1a25feec5d8f24d9e7664f497849a170da9695
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-29 17:54:42 +0530

    prisma generated

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -6,26 +6,27 @@ export const GET = async (req: Request) => {
   const { searchParams } = new URL(req.url);
 
   const page = Number(searchParams.get("page"));
-  const cat = searchParams.get("cat");
-  const featured = searchParams.get("editor");
-  const popular = searchParams.get("popular");
+  const cat = searchParams.get("cat") || null;
+  const featured = searchParams.get("editor") || null;
+  const popular = searchParams.get("popular") || null;
   const POST_PER_PAGE = 3;
-  const query = {
-    take: POST_PER_PAGE,
-    skip: POST_PER_PAGE * (page - 1),
-    where: {
-      ...(cat && { catSlug: cat }),
-      ...(featured && { featured: true }),
-    },
-    orderBy: {
-      ...(popular ? { views: "desc" } : { createdAt: "desc" }),
-    },
-    include: {
-      user: true,
-      votes: true,
-    },
-  };
+
   try {
+    const query = {
+      take: POST_PER_PAGE,
+      skip: POST_PER_PAGE * (page - 1),
+      where: {
+        ...(cat && { catSlug: cat }),
+        ...(featured && { featured: true }),
+      },
+      orderBy: {
+        ...(popular ? { views: "desc" } : { createdAt: "desc" }),
+      },
+      include: {
+        user: true,
+        votes: true,
+      },
+    };
     const [posts, count] = await prisma.$transaction([
       prisma.post.findMany(query as any),
       prisma.post.count({ where: query.where }),
